dd360be5634b561c464b616998e8e090a807ca87
ac169 <137737168+ac169@users.noreply.github.com>
2026-03-11 21:28:44 +0800

    Update 25-fftw3.sh

--- scripts.d/25-fftw3.sh
+++ scripts.d/25-fftw3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_REPO="https://github.com/FFTW/fftw3.git"
-SCRIPT_COMMIT="187045ea647ba19c55db5f503d11bd811ee6b56e"
+SCRIPT_COMMIT="7184fc796279cfa70e4ba62519ac2938054584e6"
 
 ffbuild_enabled() {
     return 0
