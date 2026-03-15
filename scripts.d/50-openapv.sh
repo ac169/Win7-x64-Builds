5ebf045dbab76a330d340d704f5bd4653ca78aaa
ac169 <137737168+ac169@users.noreply.github.com>
2026-03-16 07:46:34 +0800

    Update 50-openapv.sh

--- scripts.d/50-openapv.sh
+++ scripts.d/50-openapv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_REPO="https://github.com/AcademySoftwareFoundation/openapv.git"
-SCRIPT_COMMIT="ab235995e732402318922c77cfd965c540c67519"
+SCRIPT_COMMIT="e04b3e84743e3c6306e7c06e38bc6d369161ce91"
 
 ffbuild_enabled() {
     (( $(ffbuild_ffver) > 701 )) || return -1
